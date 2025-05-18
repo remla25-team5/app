8f66dd5f0f4846caf5425d15ce1e3991e1940714
Kevin Hoxha <K.Hoxha@student.tudelft.nl>
2025-05-18 20:35:06 +0200

    Reverse unnecessary changes

--- app-frontend/vite.config.ts
+++ app-frontend/vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// Read API target from environment variable or use localhost as default
-const API_TARGET = process.env.VITE_API_TARGET || 'http://localhost:8080'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -22,7 +19,7 @@ export default defineConfig({
     proxy: {
       // Proxy API requests to your backend server
       '/api': {
-        target: API_TARGET, // Use environment variable or default
+        target: 'http://localhost:8080', // Use environment variable or default
         changeOrigin: true,
         secure: false, // Set to true if using HTTPS and self-signed certs
       },
